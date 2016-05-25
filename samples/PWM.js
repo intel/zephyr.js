7db58dae35554f6181540b2026767fc9e6d46360
Geoff Gustafson <geoff@linux.intel.com>
2016-05-24 18:09:23 -0700

    Add functionality of Zephyr PWM sample to a third LED in PWM.js
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- samples/PWM.js
+++ samples/PWM.js
@@ -9,11 +9,45 @@ print("PWM test for brightness and blink pattern...");
 var pwm = require("pwm");
 
 // pins 3, 5, 6, and 9 are PWMs on Arduino 101, and map to channels 0-3
-var led0 = pwm.open({channel: 3});  // pin IO9
+var led0 = pwm.open({channel: 0});  // pin IO3
 
 // set timings in microseconds with *US functions
 led0.setPeriodUS(1500000);
 led0.setPulseWidthUS(1000000);
 
 // set brightness to 33% using hw cycle-based values
-var led1 = pwm.open({channel: 0, period: 3, pulseWidth: 1});  // pin IO3
+var led1 = pwm.open({channel: 1, period: 3, pulseWidth: 1});  // pin IO5
+
+// reproduce the Zephyr PWM sample in JS, changing blink timings every 4s
+var led2 = pwm.open({channel: 2});  // pin IO6
+
+var minPeriod = 1000;  // 1ms
+var maxPeriod = 1000000;  // 1s
+var period = maxPeriod;
+var dir = 0;
+
+// set initial state
+led2.setPeriodUS(period);
+led2.setPulseWidthUS(period / 2);
+
+setInterval(function () {
+    if (dir) {
+        period *= 2;
+
+        if (period > maxPeriod) {
+            dir = 0;
+            period = maxPeriod;
+        }
+    }
+    else {
+        period /= 2;
+
+        if (period < minPeriod) {
+            dir = 1;
+            period = minPeriod;
+        }
+    }
+
+    led2.setPeriodUS(period);
+    led2.setPulseWidthUS(period / 2);
+}, 4000);
