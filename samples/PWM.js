119d0a410015916d025f61e361103c78f3678857
Geoff Gustafson <geoff@linux.intel.com>
2016-05-24 17:29:40 -0700

    Fix PWM.js sample and contrast setting brightness vs. blink pattern
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>
    
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com># rebase in progress; onto 253e1ea

--- samples/PWM.js
+++ samples/PWM.js
@@ -1,12 +1,19 @@
 // Copyright (c) 2016, Intel Corporation.
 
-// Test code for Arduino 101 that uses the PWM on pin IO3 to send turn on for
-// 1s, off 1/2s, and so on.
-print("PWM test...");
+// Test code for Arduino 101 that uses the PWM on pin IO3 to set an LED's
+// brightness, and the PWM on pin IO9 to set an LED's blink pattern to on for
+// 1s, then off 1/2s, indefinitely.
+print("PWM test for brightness and blink pattern...");
 
 // import pwm module
 var pwm = require("pwm");
 
 // pins 3, 5, 6, and 9 are PWMs on Arduino 101, and map to channels 0-3
-var channel0 = pwm.open({channel: 0, period: 1500000000,
-                         dutyCycle: 1000000000});
+var led0 = pwm.open({channel: 3});  // pin IO9
+
+// set timings in microseconds with *US functions
+led0.setPeriodUS(1500000);
+led0.setPulseWidthUS(1000000);
+
+// set brightness to 33% using hw cycle-based values
+var led1 = pwm.open({channel: 0, period: 3, pulseWidth: 1});  // pin IO3
