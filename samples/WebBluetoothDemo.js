b7953238cd26e596e58c3b33211a80b17a939768
Jimmy Huang <jimmy.huang@intel.com>
2016-06-23 11:11:51 -0700

    [BLE] Updated WebBluetoothDemo.js
    Signed-off-by: Jimmy Huang <jimmy.huang@intel.com>

--- samples/WebBluetoothDemo.js
+++ samples/WebBluetoothDemo.js
@@ -20,6 +20,35 @@ TemperatureCharacteristic._onChange = null;
 
 TemperatureCharacteristic.onReadRequest = function(offset, callback) {
     print("Temperature onReadRequest called");
+    var data = new Buffer(8);
+    data.writeUInt8(this._lastValue);
+
+    // BUG: this.RESULT_SUCCESS is undefined here
+    callback(TemperatureCharacteristic.RESULT_SUCCESS, data);
+};
+
+TemperatureCharacteristic.onSubscribe = function(maxValueSize,
+                                                 updateValueCallback) {
+    print("Subscribed to temperature change.");
+    this._onChange = updateValueCallback;
+    this._lastValue = undefined;
+};
+
+TemperatureCharacteristic.onUnsubscribe = function() {
+    print("Unsubscribed to temperature change.");
+    this._onChange = null;
+};
+
+TemperatureCharacteristic.valueChange = function(value) {
+    this._lastValue = value;
+    print("Temperature change " + value);
+
+    var data = new Buffer(8);
+    data.writeUInt8(newValue);
+
+    if (this._onChange) {
+        this._onChange(data);
+    }
 };
 
 var ColorCharacteristic = new ble.Characteristic({
@@ -28,19 +57,36 @@ var ColorCharacteristic = new ble.Characteristic({
     value: null
 });
 
+ColorCharacteristic._value = new Buffer(3);
+
 ColorCharacteristic.onReadRequest = function(offset, callback) {
     print("Color onReadRequest called");
+    // BUG: this.RESULT_SUCCESS and this._value is undefined here
+    var data = new Buffer(3);
+    callback(ColorCharacteristic.RESULT_SUCCESS, data);
 };
 
 ColorCharacteristic.onWriteRequest = function(data, offset, withoutResponse,
                                               callback) {
     print("Color onWriteRequest called");
+    var value = data;
+    if (!value) {
+        // BUG: this.RESULT_SUCCESS is undefined here
+        callback(ColorCharacteristic.RESULT_SUCCESS);
+        return;
+    }
+
+    this._value = value;
+
+    print("Changing led to " + value.toString('hex'));
+    callback(ColorCharacteristic.RESULT_SUCCESS);
 };
 
-ble.on('stateChange', function(state, callback) {
+ble.on('stateChange', function(state) {
     print(state);
 
     if (state === 'poweredOn') {
+        print("POWERED ON")
         ble.startAdvertising(DEVICE_NAME, ['fc00']);
 
         // FIXME: for now this will be hard-coded in C as part of above call
