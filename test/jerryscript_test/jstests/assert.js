24910deb9f21c9bb22a2a4adf764f3c28382638f
Geoff Gustafson <geoff@linux.intel.com>
2016-08-02 15:11:38 -0700

    [test] Remove test dir which is not being used
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- test/jerryscript_test/jstests/assert.js
+++
@@ -1,9 +0,0 @@
-// Provide the assert function needed for running tests.
-function assert (condition)
-{
-    if (!condition)
-    {
-        print ("Script assertion failed");
-        throw '';
-    }
-}
