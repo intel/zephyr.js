f57996d970c3387396bd8e93b9a8f5467c611f72
Jimmy Huang <jimmy.huang@intel.com>
2016-08-03 16:51:25 -0700

    [BLE] Updated BLE sample
    Rewrite BLE sample to be a simple BLE service that allows
    the client to write some arbitrary values to be stored
    on the device and be able to read it back what he wrote.
    The client was tested using the "nRF Connect" app on Android,
    and can be tested with other BLE apps.
    
    Signed-off-by: Jimmy Huang <jimmy.huang@intel.com>

--- samples/BLE.js
+++ samples/BLE.js
@@ -1,23 +1,20 @@
+// Copyright (c) 2016, Intel Corporation.
+
+// Register a BLE echo service that expose read/write capabilities
+// write will allow client to send bytes to be stored
+// read will allow client to read back the stored value, or 0 if not found
 
 var ble = require ("ble");
 
-// setup callbacks
+var deviceName = 'BLE Test';
+
+var echoValue = new Buffer(1);
+echoValue.writeUInt8(0);
+
 ble.on('stateChange', function(state) {
-    print ('State change: ' + state);
     if (state === 'poweredOn') {
-        //TODO: Need to define what parameters should go into startAdvertising
-        ble.startAdvertising();
-    } else {
-        ble.stopAdvertising();
+        ble.startAdvertising(deviceName, ['ab00']);
     }
-});
-
-ble.on('accept', function(clientAddress) {
-    print ("Accepted connection from address: " + clientAddress);
-});
-
-ble.on('disconnect', function(clientAddress) {
-    print ("Disconnected from address: " + clientAddress);
 });
 
 ble.on('advertisingStart', function(error) {
@@ -25,9 +22,34 @@ ble.on('advertisingStart', function(error) {
         print ("Advertising start error: " + error);
         return;
     }
-    print ("Advertising start success");
-    //ToDO: Define your new service
+
+    ble.setServices([
+        new ble.PrimaryService({
+            uuid: 'ab00',
+            characteristics: [
+                new ble.Characteristic({
+                    uuid: 'ab01',
+                    properties: ['read', 'write'],
+                    descriptors: [
+                        new ble.Descriptor({
+                            uuid: '2901',
+                            value: 'Echo'
+                        })
+                    ],
+                    onReadRequest: function(offset, callback) {
+                        print("Read value: " + echoValue.toString('hex'));
+                        callback(this.RESULT_SUCCESS, echoValue);
+                    },
+                    onWriteRequest: function(data, offset, withoutResponse,
+                                             callback) {
+                        print("Write value: " + data.toString('hex'));
+                        echoValue = data;
+                        callback(this.RESULT_SUCCESS);
+                    }
+                })
+            ]
+        })
+    ]);
 });
 
-// enable ble
-ble.enable();
+print("BLE sample...");
