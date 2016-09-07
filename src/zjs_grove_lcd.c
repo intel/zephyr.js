89fd8dcdda6872698b0f010c13d99f578ad0ba86
Jimmy Huang <jimmy.huang@intel.com>
2016-09-07 10:30:45 -0700

    [IPM] Handle registering multiple IPM callbacks
    Zephyr only allow one ipm callback handler to be registered,
    so this will cause an issue where if we have more than one
    services like I2C, AIO or Grove LCD trying to call ipm_register_callback,
    where only the last one will get called, so on the x86 side,
    we'll have to register a master handler and then distribute the
    ipm messages based on each service.
    
    Signed-off-by: Jimmy Huang <jimmy.huang@intel.com>

--- src/zjs_grove_lcd.c
+++ src/zjs_grove_lcd.c
@@ -357,7 +357,7 @@ static jerry_value_t zjs_glcd_init(const jerry_value_t function_obj_val,
 jerry_value_t zjs_grove_lcd_init()
 {
     zjs_ipm_init();
-    zjs_ipm_register_callback(ipm_msg_receive_callback);
+    zjs_ipm_register_callback(MSG_ID_GLCD, ipm_msg_receive_callback);
 
     nano_sem_init(&glcd_sem);
 
--- src/zjs_ipm.c
+++ src/zjs_ipm.c
@@ -2,10 +2,10 @@
 #ifndef QEMU_BUILD
 // ipm for ARC communication
 #include <ipm/ipm_quark_se.h>
+#include <string.h>
 
 // ZJS includes
 #include "zjs_ipm.h"
-
 #include "zjs_common.h"
 
 
@@ -17,10 +17,30 @@ QUARK_SE_IPM_DEFINE(ipm_msg_receive, IPM_CHANNEL_X86_TO_ARC, QUARK_SE_IPM_INBOUN
 QUARK_SE_IPM_DEFINE(ipm_msg_send, IPM_CHANNEL_ARC_TO_X86, QUARK_SE_IPM_OUTBOUND);
 #endif
 
+struct zjs_ipm_callback {
+    uint32_t msg_id;
+    ipm_callback_t callback;
+    struct zjs_ipm_callback *next;
+};
+
 static struct device *ipm_send_dev;
 static struct device *ipm_receive_dev;
 
-void zjs_ipm_init() {
+#ifdef CONFIG_X86
+static struct zjs_ipm_callback *zjs_ipm_callbacks = NULL;
+
+static void zjs_ipm_msg_callback(void *context, uint32_t id, volatile void *data)
+{
+    for (struct zjs_ipm_callback *cb = zjs_ipm_callbacks; cb; cb = cb->next) {
+        if (cb->msg_id == id) {
+            cb->callback(context, id, data);
+        }
+    }
+}
+#endif
+
+void zjs_ipm_init()
+{
     ipm_send_dev = device_get_binding("ipm_msg_send");
 
     if (!ipm_send_dev) {
@@ -32,9 +52,16 @@ void zjs_ipm_init() {
     if (!ipm_receive_dev) {
         PRINT("Cannot find inbound ipm device!\n" );
     }
+
+#ifdef CONFIG_X86
+    // on x86 side, all ipm is routed through a single callback handler
+    ipm_register_callback(ipm_receive_dev, zjs_ipm_msg_callback, NULL);
+    ipm_set_enabled(ipm_receive_dev, 1);
+#endif
 }
 
-int zjs_ipm_send(uint32_t id, struct zjs_ipm_message *data) {
+int zjs_ipm_send(uint32_t id, struct zjs_ipm_message *data)
+{
     if (!ipm_send_dev) {
         PRINT("Cannot find outbound ipm device!\n" );
         return -1;
@@ -44,14 +71,31 @@ int zjs_ipm_send(uint32_t id, struct zjs_ipm_message *data) {
     return ipm_send(ipm_send_dev, 1, id, &data, sizeof(void *));
 }
 
-void zjs_ipm_register_callback(ipm_callback_t cb) {
+void zjs_ipm_register_callback(uint32_t msg_id, ipm_callback_t cb)
+{
     if (!ipm_receive_dev) {
         PRINT("Cannot find inbound ipm device!\n" );
         return;
     }
 
+#ifdef CONFIG_X86
+    // x86, register for ipm message that matches the MSG_ID
+    struct zjs_ipm_callback* callback = task_malloc(sizeof(struct zjs_ipm_callback));
+    if (!callback) {
+        PRINT("zjs_ipm_register_callback: failed to allocate callback\n");
+        return;
+    }
+
+    memset(callback, 0, sizeof(struct zjs_ipm_callback));
+    callback->msg_id = msg_id;
+    callback->callback = cb;
+    callback->next = zjs_ipm_callbacks;
+
+    zjs_ipm_callbacks = callback;
+#elif CONFIG_ARC
     ipm_register_callback(ipm_receive_dev, cb, NULL);
     ipm_set_enabled(ipm_receive_dev, 1);
+#endif
 }
 
 #endif // QEMU_BUILD
