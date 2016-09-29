af9cba93a2055a470b13ec854c4ebbdc3d26aec9
Miao Bin <binx.miao@intel.com>
2016-09-29 11:47:57 +0800

    Update test-aio.js
    Update AIO test case and following style standards.

--- tests/test-aio.js
+++ tests/test-aio.js
@@ -29,25 +29,25 @@ var gpio = require("gpio");
 var pins = require("arduino101_pins");
 
 
-//Interface: AIOPin open(AIOInit init);
+// Interface: AIOPin open(AIOInit init);
 var test_open_true = "Use valid argument in function open(AIOInit).";
 var test_open_false = "Use invalid argument in function open(AIOInit).";
-//use valid argument
-try{
+// use valid argument
+try {
     var A0 = aio.open({ pin: pins.A0 });
-    assert(true,test_open_true);
+    assert(true, test_open_true);
 } catch(e) {
     assert(false, test_open_true);
 }
-//use invalid argument
-try{
+// use invalid argument
+try {
     var A1 = aio.open({ pin: A1 });
-    assert(false,test_open_false);
+    assert(false, test_open_false);
 } catch(e) {
     assert(true, test_open_false + " and the error logs are as above");
 }
 
-//Function: unsigned long read()
+// Function: unsigned long read()
 var test_read_true = "Function read() works fine.";
 var test_read_false = "Function read() works abnormal.";
 
@@ -55,39 +55,39 @@ var toggle = false;
 var i = 0, times = 10, count = 0;
 var IO2 = gpio.open({ pin: pins.IO2, direction: 'out' });
 
-for (; i < times; i++){
+for (; i < times; i++) {
     toggle = !toggle;
     IO2.write(toggle);
     rawValue = A0.read();
 
-    if (toggle){
-        if (rawValue >= 4000){
+    if (toggle) {
+        if (rawValue >= 4000) {
             count ++;
         }
     } else {
-        if (rawValue <= 100){
+        if (rawValue <= 100) {
             count ++;
         }
     }
 }
-if(count == times){
-    assert(true,test_read_true);
+if (count == times) {
+    assert(true, test_read_true);
 } else {
-    assert(false,test_read_false);
+    assert(false, test_read_false);
 }
 count = 0;
 i = 0;
 
 
-//Function: void on(string eventType, ReadCallback callback);
-//Registe ReadCallback
+// Function: void on(string eventType, ReadCallback callback);
+// Registe ReadCallback
 var onCount = 0, oldOnCount = 0;
-A0.on('change', function(){
+A0.on('change', function() {
     onCount ++;
 });
 
 
-//Function: unsigned long readAsync()
+// Function: unsigned long readAsync()
 var test_read_true = "Function readAsync() works fine";
 var test_read_false = "Function readAsync() works abnormal";
 
@@ -98,22 +98,22 @@ var test_readAsync = setInterval(function () {
 
     A0.readAsync(function(rawValue) {
         print("...");
-        if (toggle){
-            if (rawValue >= 4000){
+        if (toggle) {
+            if (rawValue >= 4000) {
                 count ++;
             }
         } else {
-            if (rawValue <= 100){
+            if (rawValue <= 100) {
                 count ++;
             }
         }
 
-        //Discard ReadCallback
-        if(i == 6){
+        // Discard ReadCallback
+        if (i == 6) {
             oldOnCount = onCount;
-            if(onCount == 5){
+            if (onCount == 5) {
                 assert(true, "Function void on(string eventType, ReadCallback callback) works fine");
-            } else if(onCount < 5){
+            } else if (onCount < 5) {
                 assert(false, "Read too fast and the ReadCallback failed to be called.");
             } else {
                 assert(false, "Gets called periodically even when AIO hasn't changed.");
@@ -121,13 +121,13 @@ var test_readAsync = setInterval(function () {
             A0.on('change', null);
         }
 
-        if(i == times){
+        if (i == times) {
             clearInterval(test_readAsync);
 
-            if (count == times){
-                assert(true,test_read_true);
+            if (count == times) {
+                assert(true, test_read_true);
             } else {
-                assert(false,test_read_false);
+                assert(false, test_read_false);
             }
             count = 0;
         }
@@ -135,8 +135,8 @@ var test_readAsync = setInterval(function () {
 
 }, 1000);
 
-setTimeout(function(){
-    if(onCount == oldOnCount){
+setTimeout(function() {
+    if (onCount == oldOnCount) {
         assert(true, "Function on('change', null) unsubscribed to events on pin:A0");
     } else {
         assert(false, "Function on('change', null) unable to discard the ReadCallback.");
@@ -146,28 +146,28 @@ setTimeout(function(){
     IO2.write(true);
     A0.read();
 
-    A0.on('change', function(){
+    A0.on('change', function() {
         onCount ++;
     });
 
-    //Function: void close()
+    // Function: void close()
     A0.close();
-},12000);
+}, 12000);
 
 
-setTimeout(function(){
+setTimeout(function() {
     IO2.write(false);
     A0.read();
 
-    if(onCount == 0){
+    if (onCount == 0) {
         assert(true, "Function close() unsubscribed to events on pin:A0")
     } else {
-        assert(true,"Function close() unable to discard the ReadCallback.");
+        assert(false, "Function close() unable to discard the ReadCallback.");
     }
 
     print("TOTAL: " + passed + " of " + total + " passed");
   
-},15000);
+}, 15000);
 
 
 
