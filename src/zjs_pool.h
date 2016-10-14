ea9aa00013283df109a6957382975f4bb915e621
James Prestwood <james.prestwood@intel.com>
2016-10-14 12:21:53 -0700

    [pool] Added pool cleanup function (#290)
    Signed-off-by: James Prestwood <james.prestwood@intel.com>

--- src/zjs_pool.h
+++ src/zjs_pool.h
@@ -11,6 +11,8 @@ void* pool_malloc(uint32_t size);
 void pool_free(void* ptr);
 
 void zjs_print_pools(void);
+
+void zjs_pool_cleanup(void);
 #endif // ZJS_POOL_CONFIG
 
 #endif /* SRC_ZJS_POOL_H_ */
