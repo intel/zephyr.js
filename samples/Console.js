55db2adc7d814a68383d47014689b430420ed1a2
James Prestwood <james.prestwood@intel.com>
2016-11-04 10:15:09 -0700

    [console] Fixed console.log/error to conform to Node.js better (#344)
    Signed-off-by: James Prestwood <james.prestwood@intel.com>

---
+++ samples/Console.js
@@ -0,0 +1,12 @@
+console.log("TESTING LOG");
+console.log(1);
+console.log(-1);
+console.log(123.456);
+console.log(-123.456);
+console.log({int_val: 1});
+
+console.error("TESTING ERROR");
+console.error(1);
+console.error(123.456);
+console.error(-123.456);
+console.error({int_val: 1});
---
+++ samples/tests/Console.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2016, Intel Corporation.
+
+console.log("TESTING LOG");
+console.log(1);
+console.log(-1);
+console.log(123.456);
+console.log(-123.456);
+console.log({int_val: 1});
+console.log("multiple", 1, "arguments", -3);
+
+console.error("TESTING ERROR");
+console.error(1);
+console.error(-1);
+console.error(123.456);
+console.error(-123.456);
+console.error({int_val: 1});
+console.error("multiple", 1, "arguments", -3);
