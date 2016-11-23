2855a9cd7b5e2df01142df5aacdd1bf707d0a497
Geoff Gustafson <geoff@linux.intel.com>
2016-11-23 14:48:50 -0800

    [console] Support printing arrays, functions, null, undefined
    Also, improve handling of spaces between arguments, and improve Console.js
    test to be more thorough and clean up output.
    
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- samples/tests/Console.js
+++ samples/tests/Console.js
@@ -1,21 +1,28 @@
 // Copyright (c) 2016, Intel Corporation.
 
-console.log("TESTING LOG");
-console.log(1);
-console.log(-1);
-console.log(123.456);
-console.log(-123.456);
-console.log(true);
-console.log(false);
-console.log({int_val: 1});
-console.log("multiple", 1, "arguments", -3);
+console.log("Testing console.log...");
+console.log("   Arrays:", [], [1, 2, 3], ['a', 0, [1, 2, 3]]);
+console.log(" Booleans:", true, false);
+console.log("Functions:", function () {});
+console.log("  Numbers:", 1, -1, 123.456, -123.456);
+console.log("  Objects:", {}, {foo: 'bar'});
+console.log("  Strings:", '', "", 'abc', "def");  // expect: "  abc def"
+console.log("     Misc:", null, undefined);
 
-console.error("TESTING ERROR");
-console.error(1);
-console.error(-1);
-console.error(123.456);
-console.error(-123.456);
-console.error(true);
-console.error(false);
-console.error({int_val: 1});
-console.error("multiple", 1, "arguments", -3);
+console.log("\nTesting console.error...");
+console.error("   Arrays:", [], [1, 2, 3], ['a', 0, [1, 2, 3]]);
+console.error(" Booleans:", true, false);
+console.error("Functions:", function () {});
+console.error("  Numbers:", 1, -1, 123.456, -123.456);
+console.error("  Objects:", {}, {foo: 'bar'});
+console.error("  Strings:", '', "", 'abc', "def");  // expect: "  abc def"
+console.error("     Misc:", null, undefined);
+
+console.log("\nTesting long string...");
+var toolong =
+    "123456789012345678901234567890123456789012345678901234567890" +
+    "123456789012345678901234567890123456789012345678901234567890" +
+    "123456789012345678901234567890123456789012345678901234567890" +
+    "123456789012345678901234567890123456789012345678901234567890" +
+    "12345678901234567";  // length 257
+console.log(toolong);
--- src/zjs_console.c
+++ src/zjs_console.c
@@ -18,7 +18,7 @@
 #define IS_INT    1
 #define IS_UINT   2
 
-int is_int(jerry_value_t val) {
+static int is_int(jerry_value_t val) {
     int ret = 0;
     double n = jerry_get_number_value(val);
     ret = (n - (int) n == 0);
@@ -34,52 +34,103 @@ int is_int(jerry_value_t val) {
     }
 }
 
+static void print_value(const jerry_value_t value, FILE *out, bool deep,
+                        bool quotes)
+{
+    if (jerry_value_is_array(value)) {
+        uint32_t len = jerry_get_array_length(value);
+        if (deep) {
+            fprintf(out, "[");
+            for (int i = 0; i < len; i++) {
+                if (i) {
+                    fprintf(out, ", ");
+                }
+                jerry_value_t element = jerry_get_property_by_index(value, i);
+                print_value(element, out, false, true);
+            }
+            fprintf(out, "]");
+        }
+        else {
+            fprintf(out, "[Array - length %lu]", len);
+        }
+    }
+    else if (jerry_value_is_boolean(value)) {
+        uint8_t val = jerry_get_boolean_value(value);
+        fprintf(out, (val) ? "true" : "false");
+    }
+    else if (jerry_value_is_function(value)) {
+        fprintf(out, "[Function]");
+    }
+    else if (jerry_value_is_number(value)) {
+        int type = is_int(value);
+        if (type == IS_NUMBER) {
+#ifdef ZJS_PRINT_FLOATS
+            double num = jerry_get_number_value(argv[i]);
+            fprintf(out, "%f", num);
+#else
+            int32_t num = (int32_t)jerry_get_number_value(argv[i]);
+            fprintf(out, "[Float ~%li]", num);
+#endif
+        } else if (type == IS_UINT) {
+            uint32_t num = (uint32_t)jerry_get_number_value(value);
+            fprintf(out, "%lu", num);
+        } else if (type == IS_INT) {
+            int32_t num = (int32_t)jerry_get_number_value(value);
+            // Linux and Zephyr print int32_t's differently if %li is used
+#ifdef ZJS_LINUX_BUILD
+            fprintf(out, "%i", num);
+#else
+            fprintf(out, "%li", num);
+#endif
+        }
+    }
+    else if (jerry_value_is_null(value)) {
+        fprintf(out, "null");
+    }
+    // NOTE: important that checks for function and array were above this
+    else if (jerry_value_is_object(value)) {
+        fprintf(out, "[Object]");
+    }
+    else if (jerry_value_is_string(value)) {
+        jerry_size_t jlen = jerry_get_string_size(value);
+        if (jlen > MAX_STR_LENGTH) {
+            fprintf(out, "[String - length %lu]", jlen);
+        }
+        else {
+            char buffer[jlen + 1];
+            int wlen = jerry_string_to_char_buffer(value,
+                                                   (jerry_char_t *)buffer,
+                                                   jlen);
+            buffer[wlen] = '\0';
+            if (quotes) {
+                fprintf(out, "\"%s\"", buffer);
+            }
+            else {
+                fprintf(out, "%s", buffer);
+            }
+        }
+    }
+    else if (jerry_value_is_undefined(value)) {
+        fprintf(out, "undefined");
+    }
+    else {
+        // should never get this
+        fprintf(out, "UNKNOWN");
+    }
+}
+
 static jerry_value_t do_print(const jerry_value_t function_obj,
                               const jerry_value_t this,
                               const jerry_value_t argv[],
                               const jerry_length_t argc,
                               FILE* out)
 {
-    int i;
-    for (i = 0; i < argc; ++i) {
-        if (jerry_value_is_string(argv[i])) {
-            jerry_size_t jlen = jerry_get_string_size(argv[i]);
-            if (jlen > MAX_STR_LENGTH) {
-                ERR_PRINT("console string is too long, max length=%u\n", MAX_STR_LENGTH);
-                return ZJS_UNDEFINED;
-            }
-            char buffer[jlen + 1];
-            int wlen = jerry_string_to_char_buffer(argv[i], (jerry_char_t *)buffer, jlen);
-            buffer[wlen] = '\0';
-            fprintf(out, "%s ", buffer);
-        } else if (jerry_value_is_number(argv[i])) {
-            int type = is_int(argv[i]);
-            if (type == IS_NUMBER) {
-#ifdef ZJS_PRINT_FLOATS
-                double num = jerry_get_number_value(argv[i]);
-                fprintf(out, "%f ", num);
-#else
-                int32_t num = (int32_t)jerry_get_number_value(argv[i]);
-                fprintf(out, "[Float ~%li] ", num);
-#endif
-            } else if (type == IS_UINT) {
-                uint32_t num = (uint32_t)jerry_get_number_value(argv[i]);
-                fprintf(out, "%lu ", num);
-            } else if (type == IS_INT) {
-                int32_t num = (int32_t)jerry_get_number_value(argv[i]);
-                // Linux and Zephyr print int32_t's differently if %li is used
-#ifdef ZJS_LINUX_BUILD
-                fprintf(out, "%i ", num);
-#else
-                fprintf(out, "%li ", num);
-#endif
-            }
-        } else if (jerry_value_is_boolean(argv[i])) {
-            uint8_t val = jerry_get_boolean_value(argv[i]);
-            fprintf(out, (val) ? "true " : "false ");
-        } else if (jerry_value_is_object(argv[i])) {
-            fprintf(out, "[Object] ");
+    for (int i = 0; i < argc; i++) {
+        if (i) {
+            // insert spaces between arguments
+            fprintf(out, " ");
         }
+        print_value(argv[i], out, true, false);
     }
     fprintf(out, "\n");
     return ZJS_UNDEFINED;
