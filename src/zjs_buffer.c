0d5f40bea74c0791305c325da8d11a7bcd5d297e
Brian Jones <brian.j.jones@intel.com>
2016-12-08 16:47:35 -0800

    [buffer] Fix bug when freeing the last item (#523)
    Currently, Buffer will crash if it tries to free the last item,
    because freeing it tries to look at the ->next of NULL. Instead,
    return once the handle has been freed.

--- src/zjs_buffer.c
+++ src/zjs_buffer.c
@@ -267,6 +267,7 @@ static void zjs_buffer_callback_free(uintptr_t handle)
             zjs_free((*pItem)->buffer);
             *pItem = (*pItem)->next;
             zjs_free((void *)handle);
+            return;
         }
         pItem = &(*pItem)->next;
     }
