f55b16fc9540aee7a40dc390bd1ba5e02960568c
Brian Jones <brian.j.jones@intel.com>
2016-12-28 15:02:50 -0800

    Adding prototype code to the ipm version of I2C

--- src/zjs_i2c_ipm.c
+++ src/zjs_i2c_ipm.c
@@ -18,6 +18,7 @@
 #define ZJS_I2C_TIMEOUT_TICKS                      500
 
 static struct k_sem i2c_sem;
+static jerry_value_t zjs_i2c_prototype;
 
 static bool zjs_i2c_ipm_send_sync(zjs_ipm_message_t* send,
                                   zjs_ipm_message_t* result) {
@@ -300,11 +301,7 @@ static jerry_value_t zjs_i2c_open(const jerry_value_t function_obj,
     if (reply.error_code == 0) {
         // create the I2C object
         jerry_value_t i2c_obj = jerry_create_object();
-        zjs_obj_add_function(i2c_obj, zjs_i2c_read, "read");
-        zjs_obj_add_function(i2c_obj, zjs_i2c_burst_read, "burstRead");
-        zjs_obj_add_function(i2c_obj, zjs_i2c_write, "write");
-        zjs_obj_add_function(i2c_obj, zjs_i2c_abort, "abort");
-        zjs_obj_add_function(i2c_obj, zjs_i2c_close, "close");
+        jerry_set_prototype(i2c_obj, zjs_i2c_prototype);
         zjs_obj_add_number(i2c_obj, bus, "bus");
         zjs_obj_add_number(i2c_obj, speed, "speed");
         return i2c_obj;
@@ -321,10 +318,26 @@ jerry_value_t zjs_i2c_init()
 
     k_sem_init(&i2c_sem, 0, 1);
 
+    zjs_native_func_t array[] = {
+        { zjs_i2c_read, "read" },
+        { zjs_i2c_burst_read, "burstRead" },
+        { zjs_i2c_write, "write" },
+        { zjs_i2c_abort, "abort" },
+        { zjs_i2c_close, "close" },
+        { NULL, NULL }
+    };
+    zjs_i2c_prototype = jerry_create_object();
+    zjs_obj_add_functions(zjs_i2c_prototype, array);
+
     // create global I2C object
     jerry_value_t i2c_obj = jerry_create_object();
     zjs_obj_add_function(i2c_obj, zjs_i2c_open, "open");
     return i2c_obj;
+}
+
+void zjs_i2c_cleanup()
+{
+    jerry_release_value(zjs_i2c_prototype);
 }
 
 #endif // QEMU_BUILD
