3ccc314557315e3cbb83076a87bcec2045f80f94
Geoff Gustafson <geoff@linux.intel.com>
2017-01-19 15:51:14 -0800

    [console] Simplify time/timeEnd implementation with JS object (#618)
    Works without malloc or adding a JS object for each timer.
    Also, add cleanup function to release the global time object.
    
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- samples/Console.js
+++ samples/Console.js
@@ -1,18 +1,19 @@
-console.log("TESTING LOG");
+// Copyright (c) 2016-2017, Intel Corporation.
+
+console.log("TESTING LOG...");
 console.log(1);
 console.log(-1);
 console.log(123.456);
 console.log(-123.456);
 console.log({int_val: 1});
 
-console.error("TESTING ERROR");
+console.error("\nTESTING ERROR...");
 console.error(1);
 console.error(123.456);
 console.error(-123.456);
 console.error({int_val: 1});
 
-console.log("TESTING TIME");
-
+console.log("\nTESTING TIME...");
 console.time('timer-1');
 setTimeout(function() {
     console.timeEnd('timer-1');
