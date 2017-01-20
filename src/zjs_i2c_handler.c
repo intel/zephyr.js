92c4f58e8e9de960cb93b5377e1ee7b16d9e0bf0
Brian Jones <brian.j.jones@intel.com>
2017-01-20 13:16:09 -0800

    [i2c] Reorganize code to make future maintenance easier (#613)
    Both ARC and non-ARC boards will now use the same base code.  The
    IPM logic has been moved to its own file.  Now any changed needed
    for I2C can address either the I2C code or the IPM code.  This
    will remove the need to update two code bases with any future
    changes.

---
+++ src/zjs_i2c_handler.c
@@ -0,0 +1,115 @@
+// Copyright (c) 2016-2017, Intel Corporation.
+#include "zjs_i2c_handler.h"
+#include "zjs_common.h"
+
+#define I2C_0 0
+#define I2C_1 1
+#define MAX_I2C_BUS 2
+
+static struct device *i2c_device[MAX_I2C_BUS];
+
+uint8_t zjs_i2c_handle_open(uint8_t msg_bus)
+{
+    uint8_t error_code = 0;
+    if (msg_bus < MAX_I2C_BUS) {
+        char bus[6];
+        snprintf(bus, 6, "I2C_%i", msg_bus);
+        i2c_device[msg_bus] = device_get_binding(bus);
+
+        if (!i2c_device[msg_bus]) {
+                ZJS_PRINT("I2C bus %s not found.\n", bus);
+                error_code = 1;
+        } else {
+            /* TODO remove these hard coded numbers
+             * once the config API is made */
+            union dev_config cfg;
+            cfg.raw = 0;
+            cfg.bits.use_10_bit_addr = 0;
+            cfg.bits.speed = I2C_SPEED_STANDARD;
+            cfg.bits.is_master_device = 1;
+
+            if (i2c_configure(i2c_device[msg_bus], cfg.raw) != 0) {
+                ZJS_PRINT("I2C bus %s configure failed.\n", bus);
+                error_code = 1;
+            }
+        }
+    } else {
+        ZJS_PRINT("I2C bus I2C_%i is not a valid I2C bus.\n", msg_bus);
+        error_code = 1;
+    }
+    return error_code;
+}
+
+uint8_t zjs_i2c_handle_write(uint8_t msg_bus, uint8_t *data,
+                             uint32_t length, uint16_t address)
+{
+    uint8_t error_code = 0;
+    if (msg_bus < MAX_I2C_BUS) {
+        // Write has to come after an Open I2C message
+        if (i2c_device[msg_bus]) {
+            error_code = i2c_write(i2c_device[msg_bus],
+                                   data,
+                                   length,
+                                   address);
+
+                if (error_code != 0)
+                    ZJS_PRINT("i2c_write failed!\n");
+        }
+        else {
+            ZJS_PRINT("no I2C device is ready yet\n");
+            error_code = 1;
+        }
+    }
+    return error_code;
+}
+
+uint8_t zjs_i2c_handle_read(uint8_t msg_bus, uint8_t *data,
+                            uint32_t length, uint16_t address)
+{
+    uint8_t error_code = 0;
+    if (msg_bus < MAX_I2C_BUS) {
+        // Read has to come after an Open I2C message
+        if (i2c_device[msg_bus]) {
+
+            int reply = i2c_read(i2c_device[msg_bus],
+                                 data,
+                                 length,
+                                 address);
+
+            if (reply < 0) {
+                error_code = 1;
+            }
+        }
+        else {
+            ZJS_PRINT("No I2C device is ready yet\n");
+            error_code = 1;
+        }
+    }
+    return error_code;
+}
+
+uint8_t zjs_i2c_handle_burst_read(uint8_t msg_bus, uint8_t *data,
+                                  uint32_t length, uint16_t address,
+                                  uint16_t register_addr)
+{
+    uint8_t error_code = 0;
+    if (msg_bus < MAX_I2C_BUS) {
+        // Burst read has to come after an Open I2C message
+        if (i2c_device[msg_bus]) {
+           int reply = i2c_burst_read(i2c_device[msg_bus],
+                                      address,
+                                      register_addr,
+                                      data,
+                                      length);
+
+            if (reply < 0) {
+                error_code = 1;
+            }
+        }
+        else {
+            ZJS_PRINT("No I2C device is ready yet\n");
+            error_code = 1;
+        }
+    }
+    return error_code;
+}
---
+++ src/zjs_i2c_ipm_handler.c
@@ -0,0 +1,144 @@
+// Copyright (c) 2016-2017, Intel Corporation.
+#include "zjs_i2c_handler.h"
+#include "zjs_ipm.h"
+#include "zjs_common.h"
+
+static struct k_sem i2c_sem;
+#define ZJS_I2C_TIMEOUT_TICKS                      5000
+
+static bool zjs_i2c_ipm_send_sync(zjs_ipm_message_t* send,
+                                  zjs_ipm_message_t* result) {
+    send->id = MSG_ID_I2C;
+    send->flags = 0 | MSG_SYNC_FLAG;
+    send->user_data = (void *)result;
+    send->error_code = ERROR_IPM_NONE;
+
+    if (zjs_ipm_send(MSG_ID_I2C, send) != 0) {
+        ERR_PRINT("failed to send message\n");
+        return false;
+    }
+
+    // block until reply or timeout
+    if (k_sem_take(&i2c_sem, ZJS_I2C_TIMEOUT_TICKS)) {
+        ERR_PRINT("ipm timed out\n");
+        return false;
+    }
+
+    return true;
+}
+
+static void ipm_msg_receive_callback(void *context, uint32_t id,
+                                     volatile void *data)
+{
+    if (id != MSG_ID_I2C)
+        return;
+
+    zjs_ipm_message_t *msg = (zjs_ipm_message_t*)(*(uintptr_t *)data);
+
+    if (msg->flags & MSG_SYNC_FLAG) {
+         zjs_ipm_message_t *result = (zjs_ipm_message_t*)msg->user_data;
+        // synchronous ipm, copy the results
+        if (result)
+            memcpy(result, msg, sizeof(zjs_ipm_message_t));
+
+        // un-block sync api
+        k_sem_give(&i2c_sem);
+    } else {
+        // asynchronous ipm, should not get here
+        ERR_PRINT("async message received\n");
+    }
+}
+
+void zjs_i2c_ipm_init() {
+    zjs_ipm_init();
+    zjs_ipm_register_callback(MSG_ID_I2C, ipm_msg_receive_callback);
+    k_sem_init(&i2c_sem, 0, 1);
+}
+
+uint8_t zjs_i2c_handle_write (uint8_t msg_bus, uint8_t *data, uint32_t length,
+                              uint16_t address)
+{
+    zjs_ipm_message_t send;
+    zjs_ipm_message_t reply;
+
+    send.type = TYPE_I2C_WRITE;
+    send.data.i2c.bus = (uint8_t)msg_bus;
+    send.data.i2c.data = data;
+    send.data.i2c.address = (uint16_t)address;
+    send.data.i2c.length = length;
+
+
+    // We have ARC send it via ipm
+    bool success = zjs_i2c_ipm_send_sync(&send, &reply);
+
+    if (!success) {
+        ERR_PRINT("zjs_i2c_write: ipm message failed or timed out!");
+    }
+
+    return send.error_code;
+}
+
+uint8_t zjs_i2c_handle_open (uint8_t msg_bus)
+{
+    // send IPM message to the ARC side
+    zjs_ipm_message_t send;
+    zjs_ipm_message_t reply;
+
+    send.type = TYPE_I2C_OPEN;
+    send.data.i2c.bus = (uint8_t)msg_bus;
+    send.data.i2c.speed = (uint8_t)I2C_SPEED_STANDARD;
+
+    bool success = zjs_i2c_ipm_send_sync(&send, &reply);
+
+    if (!success) {
+        ERR_PRINT("zjs_i2c_write: ipm message failed or timed out!");
+    }
+
+    if (send.error_code) {
+        ERR_PRINT("zjs_i2c_open: Failed to open I2C bus");
+    }
+
+    return send.error_code;
+}
+
+uint8_t zjs_i2c_handle_read (uint8_t msg_bus, uint8_t *data, uint32_t length,
+                             uint16_t address)
+{
+    zjs_ipm_message_t send;
+    zjs_ipm_message_t reply;
+
+    send.type = TYPE_I2C_READ;
+    send.data.i2c.bus = (uint8_t)msg_bus;
+    send.data.i2c.data = data;
+    send.data.i2c.address = (uint16_t)address;
+    send.data.i2c.length = length;
+
+    bool success = zjs_i2c_ipm_send_sync(&send, &reply);
+
+    if (!success) {
+        ERR_PRINT("zjs_i2c_read_base: ipm message failed or timed out!");
+    }
+    return send.error_code;
+}
+
+uint8_t zjs_i2c_handle_burst_read (uint8_t msg_bus, uint8_t *data,
+                                   uint32_t length, uint16_t address,
+                                   uint16_t register_addr)
+{
+    zjs_ipm_message_t send;
+    zjs_ipm_message_t reply;
+
+    send.type = TYPE_I2C_BURST_READ;
+    send.data.i2c.bus = (uint8_t)msg_bus;
+    send.data.i2c.data = data;
+    send.data.i2c.address = (uint16_t)address;
+    send.data.i2c.register_addr = register_addr;
+    send.data.i2c.length = length;
+
+    bool success = zjs_i2c_ipm_send_sync(&send, &reply);
+
+    if (!success) {
+        ERR_PRINT("zjs_i2c_read_base: ipm message failed or timed out!");
+    }
+    return send.error_code;
+}
