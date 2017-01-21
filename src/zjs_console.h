62fd1b952450c788920ea45d76f068f06ebad983
Geoff Gustafson <geoff@linux.intel.com>
2017-01-20 16:01:27 -0800

    [console] Fix warning in header file and add documentation (#632)
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- src/zjs_console.h
+++ src/zjs_console.h
@@ -1,3 +1,11 @@
 // Copyright (c) 2016, Intel Corporation.
 
-void zjs_console_init(void);
+/**
+ * Initialize the console module, or reinitialize after cleanup
+ *
+ * @return CONSOLE API object
+ */
+void zjs_console_init();
+
+/** Release resources held by the console module */
+void zjs_console_cleanup();
