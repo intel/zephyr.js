722cc13e2e23a514babe5c3ef02514ff658b9ab2
James Prestwood <james.prestwood@intel.com>
2017-02-15 11:08:24 -0800

    fixed bad return
    Signed-off-by: James Prestwood <james.prestwood@intel.com>

--- src/zjs_ocf_ble.c
+++ src/zjs_ocf_ble.c
@@ -92,7 +92,8 @@ void zjs_init_ocf_ble()
     };
 
     if (str2bt_addr_le(default_ble, "random", &id_addr) < 0) {
-        return zjs_error("bad BLE address string");
+        ERR_PRINT("bad BLE address string");
+        return;
     }
     DBG_PRINT("BLE addr is set to: %s\n", default_ble);
     BT_ADDR_SET_STATIC(&id_addr.a);
