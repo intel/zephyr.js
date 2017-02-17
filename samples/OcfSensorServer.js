1326bef40ae17ad220e6a2cf543b07bdfec31e64
James Prestwood <james.prestwood@intel.com>
2017-02-16 17:39:01 -0800

    [ocf] Changed sensor sever to remove unused properties
    Signed-off-by: James Prestwood <james.prestwood@intel.com>

--- samples/OcfSensorServer.js
+++ samples/OcfSensorServer.js
@@ -13,15 +13,13 @@ var pinA = aio.open({ device: 0, pin: pins.A0 });
 console.log("Started OCF server");
 
 var MyProperties = {
-    state: true,
-    power: 10,
     sensor: null,
     light: false
 }
 
 var resourceInit = {
-    resourcePath: "/a/light",
-    resourceTypes: ["core.light"],
+    resourcePath: "/a/sensor",
+    resourceTypes: ["core.sensor"],
     interfaces: ["/oic/if/rw"],
     discoverable: true,
     observable: true,
@@ -34,7 +32,6 @@ server.register(resourceInit).then(function(resource) {
     console.log("Registered resource");
     MyResource = resource;
     server.on('retrieve', function(request, observe) {
-        MyProperties.state = (MyProperties.state) ? false : true;
         MyProperties.sensor = pinA.read();
         console.log("on('retrieve'): request.target.resourcePath=" +
                 request.target.resourcePath + " observe=" + observe);
@@ -47,9 +44,12 @@ server.register(resourceInit).then(function(resource) {
     server.on('update', function(request) {
         console.log("on('update'): request.target.resourcePath=" + request.target.resourcePath);
         if (request.resource.properties) {
-            console.log("properties.state=" + request.resource.properties.state);
-            led.write(request.resource.properties.light);
-            MyProperties = request.resource.properties;
+            var recvProps = request.resource.properties;
+            if (recvProps.light != undefined) {
+                MyProperties.light = recvProps.light;
+                led.write(recvProps.light);
+                console.log("properties.light=" + recvProps.light);
+            }
         } else {
             console.log("request.properties does not exist");
         }
