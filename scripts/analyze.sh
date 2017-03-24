a700e932251f47937fbab2b7941ef8cfd5ac38e9
Geoff Gustafson <geoff@linux.intel.com>
2017-03-23 19:46:49 -0700

    [trlite] Add support for running tests in parallel
    You run trlite -j (but with no argument for now) to have it execute all
    four "VMs" in parallel processes. It runs them silently but prints out
    the last few lines if there are errors, and points you to the logs for
    those that failed. (They are in /tmp/trlite-log*.)
    
    To do this, it creates four separate tree clone directories, .trlite1
    through .trlite4 in your ZJS_BASE. It will leave the ones there that had
    a failure in case you need to debug further.
    
    To support the parallel building, I had to fix a problem in analyze.sh
    and Makefile where it was using a constant temp file name /tmp/zjs.js
    which would then clash across the processes.
    
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -15,10 +15,14 @@ if [ ! -e $2 ]; then
     exit
 fi
 
-if [ -f "/tmp/zjs.js" ]; then
-    rm /tmp/zjs.js
+JSTMPFILE=/tmp/zjs-analyze.js.$$
+if [ -f $JSTMPFILE ]; then
+    rm $JSTMPFILE
 fi
 
+PRJFILE=prj.conf.tmp
+ARCPRJFILE=arc/prj.conf.tmp
+CONFFILE=zjs.conf.tmp
 
 MODULES=''
 BOARD=$1
@@ -27,31 +31,30 @@ CONFIG=$3
 DEV=$4
 SDK_VERSION=$(cat ${ZEPHYR_SDK_INSTALL_DIR}/sdk_version)
 
-echo "# Modules found in $SCRIPT:" > prj.conf.tmp
-echo "# Modules found in $SCRIPT:" > arc/prj.conf.tmp
+echo "# Modules found in $SCRIPT:" > $PRJFILE
+echo "# Modules found in $SCRIPT:" > $ARCPRJFILE
 tmpstr="# ZJS flags set by analyze.js"
 
 if [ -n "$CONFIG" ]; then
     tmpstr+=" and $CONFIG"
 fi
 
-echo $tmpstr > zjs.conf.tmp
+echo $tmpstr > $CONFFILE
 
 function check_for_js_require()
 {
-
     js_files=$(grep "require *( *['\"].*\.js['\"] *)" $SCRIPT | grep -o "['\"].*\.js['\"]" | tr -d \'\")
 
     for file in $js_files
     do
          >&2 echo "Javascript module included : $file"
          # Add the module JS to the temporary JS file
-         cat "$ZJS_BASE/modules/$file" >> /tmp/zjs.js
+         cat "$ZJS_BASE/modules/$file" >> $JSTMPFILE
     done
 
     # Add the primary JS file to the temporary JS file
-    cat "$SCRIPT" >> /tmp/zjs.js
-    SCRIPT=/tmp/zjs.js
+    cat "$SCRIPT" >> $JSTMPFILE
+    SCRIPT=$JSTMPFILE
 }
 
 function check_for_require()
@@ -66,8 +69,8 @@ function check_for_require()
 function check_config_file()
 {
     # effects: checks $1 for uncommented module to include from #CONFIG
-    #          If found, it will be put in zjs.conf.tmp and inserted into src/Makefile
-
+    #          If found, it will be put in zjs.conf.tmp and inserted into
+    #          src/Makefile
     if [ -n "$CONFIG" ]; then
         grep -v '^#' < $CONFIG | grep $1='y\|m' >> /dev/null
         return $?
@@ -92,30 +95,30 @@ check_for_js_require
 if check_for_require dgram || check_config_file ZJS_DGRAM; then
     >&2 echo Using module: Dgram
     MODULES+=" -DBUILD_MODULE_DGRAM -DBUILD_MODULE_BUFFER"
-    echo "export ZJS_DGRAM=y" >> zjs.conf.tmp
-    echo "export ZJS_BUFFER=y" >> zjs.conf.tmp
-    echo "CONFIG_NETWORKING=y" >> prj.conf.tmp
-    echo "CONFIG_NET_IPV6=y" >> prj.conf.tmp
-    echo "CONFIG_NET_IPV4=y" >> prj.conf.tmp
-    echo "CONFIG_NET_UDP=y" >> prj.conf.tmp
+    echo "export ZJS_DGRAM=y" >> $CONFFILE
+    echo "export ZJS_BUFFER=y" >> $CONFFILE
+    echo "CONFIG_NETWORKING=y" >> $PRJFILE
+    echo "CONFIG_NET_IPV6=y" >> $PRJFILE
+    echo "CONFIG_NET_IPV4=y" >> $PRJFILE
+    echo "CONFIG_NET_UDP=y" >> $PRJFILE
 
     # Buffer number options - these should be handled more generally
-    echo "CONFIG_NET_NBUF_RX_COUNT=10" >> prj.conf.tmp
-    echo "CONFIG_NET_NBUF_TX_COUNT=10" >> prj.conf.tmp
-    echo "CONFIG_NET_NBUF_DATA_COUNT=30" >> prj.conf.tmp
+    echo "CONFIG_NET_NBUF_RX_COUNT=10" >> $PRJFILE
+    echo "CONFIG_NET_NBUF_TX_COUNT=10" >> $PRJFILE
+    echo "CONFIG_NET_NBUF_DATA_COUNT=30" >> $PRJFILE
 
     # Debug options
-    echo "CONFIG_TEST_RANDOM_GENERATOR=y" >> prj.conf.tmp
-    echo "CONFIG_NET_SLIP_TAP=y" >> prj.conf.tmp
-    echo "CONFIG_PRINTK=y" >> prj.conf.tmp
-    echo "CONFIG_NET_LOG=y" >> prj.conf.tmp
-    echo "CONFIG_NET_STATISTICS=y" >> prj.conf.tmp
+    echo "CONFIG_TEST_RANDOM_GENERATOR=y" >> $PRJFILE
+    echo "CONFIG_NET_SLIP_TAP=y" >> $PRJFILE
+    echo "CONFIG_PRINTK=y" >> $PRJFILE
+    echo "CONFIG_NET_LOG=y" >> $PRJFILE
+    echo "CONFIG_NET_STATISTICS=y" >> $PRJFILE
 fi
 
 if check_for_require events || check_config_file ZJS_EVENTS; then
     >&2 echo Using module: Events
     MODULES+=" -DBUILD_MODULE_EVENTS"
-    echo "export ZJS_EVENTS=y" >> zjs.conf.tmp
+    echo "export ZJS_EVENTS=y" >> $CONFFILE
 fi
 
 if check_for_require ocf || check_config_file ZJS_OCF; then
@@ -143,132 +146,132 @@ if check_for_require ocf || check_config_file ZJS_OCF; then
     fi
 
     if [ $BOARD = "qemu_x86" ]; then
-        echo "CONFIG_NET_LOG=y" >> prj.conf.tmp
-        echo "CONFIG_NET_SLIP=y" >> prj.conf.tmp
-        echo "CONFIG_NET_SLIP_TAP=y" >> prj.conf.tmp
+        echo "CONFIG_NET_LOG=y" >> $PRJFILE
+        echo "CONFIG_NET_SLIP=y" >> $PRJFILE
+        echo "CONFIG_NET_SLIP_TAP=y" >> $PRJFILE
     fi
-    echo "CONFIG_NETWORKING=y" >> prj.conf.tmp
-    echo "CONFIG_NET_IPV6=y" >> prj.conf.tmp
-    echo "CONFIG_NET_UDP=y" >> prj.conf.tmp
-    echo "CONFIG_TEST_RANDOM_GENERATOR=y" >> prj.conf.tmp
-    echo "CONFIG_INIT_STACKS=y" >> prj.conf.tmp
-    echo "CONFIG_NET_NBUF_RX_COUNT=14" >> prj.conf.tmp
-    echo "CONFIG_NET_NBUF_TX_COUNT=14" >> prj.conf.tmp
-    echo "CONFIG_NET_IF_UNICAST_IPV6_ADDR_COUNT=1" >> prj.conf.tmp
-    echo "CONFIG_NET_IF_MCAST_IPV6_ADDR_COUNT=1" >> prj.conf.tmp
-    echo "CONFIG_NET_MAX_CONTEXTS=3" >> prj.conf.tmp
+    echo "CONFIG_NETWORKING=y" >> $PRJFILE
+    echo "CONFIG_NET_IPV6=y" >> $PRJFILE
+    echo "CONFIG_NET_UDP=y" >> $PRJFILE
+    echo "CONFIG_TEST_RANDOM_GENERATOR=y" >> $PRJFILE
+    echo "CONFIG_INIT_STACKS=y" >> $PRJFILE
+    echo "CONFIG_NET_NBUF_RX_COUNT=14" >> $PRJFILE
+    echo "CONFIG_NET_NBUF_TX_COUNT=14" >> $PRJFILE
+    echo "CONFIG_NET_IF_UNICAST_IPV6_ADDR_COUNT=1" >> $PRJFILE
+    echo "CONFIG_NET_IF_MCAST_IPV6_ADDR_COUNT=1" >> $PRJFILE
+    echo "CONFIG_NET_MAX_CONTEXTS=3" >> $PRJFILE
 
-    echo "CONFIG_BLUETOOTH=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_SMP=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_SIGNING=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_PERIPHERAL=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_L2CAP_DYNAMIC_CHANNEL=y" >> prj.conf.tmp
-    echo "CONFIG_NETWORKING_WITH_6LOWPAN=y" >> prj.conf.tmp
-    echo "CONFIG_6LOWPAN_COMPRESSION_IPHC=y" >> prj.conf.tmp
-    echo "CONFIG_NET_L2_BLUETOOTH_ZEP1656=y" >> prj.conf.tmp
-    echo "CONFIG_NET_L2_BLUETOOTH=y" >> prj.conf.tmp
+    echo "CONFIG_BLUETOOTH=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_SMP=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_SIGNING=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_PERIPHERAL=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_L2CAP_DYNAMIC_CHANNEL=y" >> $PRJFILE
+    echo "CONFIG_NETWORKING_WITH_6LOWPAN=y" >> $PRJFILE
+    echo "CONFIG_6LOWPAN_COMPRESSION_IPHC=y" >> $PRJFILE
+    echo "CONFIG_NET_L2_BLUETOOTH_ZEP1656=y" >> $PRJFILE
+    echo "CONFIG_NET_L2_BLUETOOTH=y" >> $PRJFILE
 
-    echo "export ZJS_OCF=y" >> zjs.conf.tmp
-    echo "export ZJS_EVENTS=y" >> zjs.conf.tmp
+    echo "export ZJS_OCF=y" >> $CONFFILE
+    echo "export ZJS_EVENTS=y" >> $CONFFILE
 fi
 
 if check_for_require gpio || check_config_file ZJS_GPIO; then
     MODULES+=" -DBUILD_MODULE_GPIO"
-    echo "CONFIG_GPIO=y" >> prj.conf.tmp
-    echo "export ZJS_GPIO=y" >> zjs.conf.tmp
+    echo "CONFIG_GPIO=y" >> $PRJFILE
+    echo "export ZJS_GPIO=y" >> $CONFFILE
 fi
 
 if check_for_require performance || check_config_file ZJS_PERFORMANCE; then
     >&2 echo Using module: Performance
     MODULES+=" -DBUILD_MODULE_PERFORMANCE"
-    echo "export ZJS_PERFORMANCE=y" >> zjs.conf.tmp
+    echo "export ZJS_PERFORMANCE=y" >> $CONFFILE
 fi
 
 if check_for_require pwm || check_config_file ZJS_PWM; then
     >&2 echo Using module: PWM
     MODULES+=" -DBUILD_MODULE_PWM"
-    echo "CONFIG_PWM=y" >> prj.conf.tmp
-    echo "CONFIG_PWM_QMSI_NUM_PORTS=4" >> prj.conf.tmp
-    echo "export ZJS_PWM=y" >> zjs.conf.tmp
+    echo "CONFIG_PWM=y" >> $PRJFILE
+    echo "CONFIG_PWM_QMSI_NUM_PORTS=4" >> $PRJFILE
+    echo "export ZJS_PWM=y" >> $CONFFILE
 fi
 
 if check_for_require uart || check_config_file ZJS_UART; then
     >&2 echo Using module: UART
     if [ $BOARD = "arduino_101" ]; then
-        echo "CONFIG_GPIO=y" >> prj.conf.tmp
-        echo "CONFIG_USB=y" >> prj.conf.tmp
-        echo "CONFIG_USB_DW=y" >> prj.conf.tmp
-        echo "CONFIG_USB_DEVICE_STACK=y" >> prj.conf.tmp
-        echo "CONFIG_SYS_LOG_USB_DW_LEVEL=0" >> prj.conf.tmp
+        echo "CONFIG_GPIO=y" >> $PRJFILE
+        echo "CONFIG_USB=y" >> $PRJFILE
+        echo "CONFIG_USB_DW=y" >> $PRJFILE
+        echo "CONFIG_USB_DEVICE_STACK=y" >> $PRJFILE
+        echo "CONFIG_SYS_LOG_USB_DW_LEVEL=0" >> $PRJFILE
         if [ "$DEV" != "ashell" ]; then
-            echo "CONFIG_USB_CDC_ACM=y" >> prj.conf.tmp
+            echo "CONFIG_USB_CDC_ACM=y" >> $PRJFILE
         fi
-        echo "CONFIG_SYS_LOG_USB_LEVEL=0" >> prj.conf.tmp
-        echo "CONFIG_SERIAL=y" >> prj.conf.tmp
-        echo "CONFIG_UART_LINE_CTRL=y" >> prj.conf.tmp
+        echo "CONFIG_SYS_LOG_USB_LEVEL=0" >> $PRJFILE
+        echo "CONFIG_SERIAL=y" >> $PRJFILE
+        echo "CONFIG_UART_LINE_CTRL=y" >> $PRJFILE
     fi
-    echo "CONFIG_UART_INTERRUPT_DRIVEN=y" >> prj.conf.tmp
+    echo "CONFIG_UART_INTERRUPT_DRIVEN=y" >> $PRJFILE
     MODULES+=" -DBUILD_MODULE_UART"
     MODULES+=" -DBUILD_MODULE_EVENTS"
-    echo "export ZJS_EVENTS=y" >> zjs.conf.tmp
-    echo "export ZJS_UART=y" >> zjs.conf.tmp
+    echo "export ZJS_EVENTS=y" >> $CONFFILE
+    echo "export ZJS_UART=y" >> $CONFFILE
     buffer=true;
 fi
 
 if check_for_require ble || check_config_file ZJS_BLE; then
     >&2 echo Using module: BLE
     MODULES+=" -DBUILD_MODULE_BLE"
-    echo "CONFIG_BLUETOOTH=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_STACK_NBLE=y" >> prj.conf.tmp
-    echo "CONFIG_NBLE=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_SMP=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_PERIPHERAL=y" >> prj.conf.tmp
-    echo "CONFIG_BLUETOOTH_GATT_DYNAMIC_DB=y" >> prj.conf.tmp
+    echo "CONFIG_BLUETOOTH=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_STACK_NBLE=y" >> $PRJFILE
+    echo "CONFIG_NBLE=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_SMP=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_PERIPHERAL=y" >> $PRJFILE
+    echo "CONFIG_BLUETOOTH_GATT_DYNAMIC_DB=y" >> $PRJFILE
     MODULES+=" -DBUILD_MODULE_EVENTS"
-    echo "export ZJS_EVENTS=y" >> zjs.conf.tmp
-    echo "export ZJS_BLE=y" >> zjs.conf.tmp
+    echo "export ZJS_EVENTS=y" >> $CONFFILE
+    echo "export ZJS_BLE=y" >> $CONFFILE
     buffer=true;
 fi
 
 if check_for_require aio || check_config_file ZJS_AIO; then
     >&2 echo Using module: AIO
     MODULES+=" -DBUILD_MODULE_AIO"
-    echo "CONFIG_ADC=y" >> arc/prj.conf.tmp
+    echo "CONFIG_ADC=y" >> $ARCPRJFILE
     # Workaround for the Zephyr issue ZEP-1882: ADC doesn't work with
     # SDK 0.9 due to some compiler optimization, so enable debug mode.
     if [ $SDK_VERSION = "0.9" ]; then
         >&2 echo Warning: Zephyr SDK 0.9 detected, compiling in debug mode!
-        echo "CONFIG_DEBUG=y" >> arc/prj.conf.tmp
+        echo "CONFIG_DEBUG=y" >> $ARCPRJFILE
     fi
-    echo "export ZJS_AIO=y" >> zjs.conf.tmp
+    echo "export ZJS_AIO=y" >> $CONFFILE
 fi
 
 if check_for_require fs || check_config_file ZJS_FS; then
     >&2 echo Using module: FS
     MODULES+=" -DBUILD_MODULE_FS -DBUILD_MODULE_BUFFER"
     if [ $BOARD = "arduino_101" ]; then
-        echo "CONFIG_FS_FAT_FLASH_DISK_W25QXXDV=y" >> prj.conf.tmp
+        echo "CONFIG_FS_FAT_FLASH_DISK_W25QXXDV=y" >> $PRJFILE
     fi
-    echo "CONFIG_FILE_SYSTEM=y" >> prj.conf.tmp
-    echo "CONFIG_FILE_SYSTEM_FAT=y" >> prj.conf.tmp
-    echo "CONFIG_DISK_ACCESS_FLASH=y" >> prj.conf.tmp
+    echo "CONFIG_FILE_SYSTEM=y" >> $PRJFILE
+    echo "CONFIG_FILE_SYSTEM_FAT=y" >> $PRJFILE
+    echo "CONFIG_DISK_ACCESS_FLASH=y" >> $PRJFILE
 
-    echo "CONFIG_FLASH=y" >> prj.conf.tmp
-    echo "CONFIG_SPI=y" >> prj.conf.tmp
-    echo "CONFIG_GPIO=y" >> prj.conf.tmp
-    echo "export ZJS_FS=y" >> zjs.conf.tmp
-    echo "export ZJS_BUFFER=y" >> zjs.conf.tmp
+    echo "CONFIG_FLASH=y" >> $PRJFILE
+    echo "CONFIG_SPI=y" >> $PRJFILE
+    echo "CONFIG_GPIO=y" >> $PRJFILE
+    echo "export ZJS_FS=y" >> $CONFFILE
+    echo "export ZJS_BUFFER=y" >> $CONFFILE
 fi
 
 if check_for_require i2c || check_config_file ZJS_I2C; then
     >&2 echo Using module: I2C
     MODULES+=" -DBUILD_MODULE_I2C"
     if [ $BOARD = "arduino_101" ]; then
-        echo "CONFIG_I2C=y" >> arc/prj.conf.tmp
+        echo "CONFIG_I2C=y" >> $ARCPRJFILE
     else
-        echo "CONFIG_I2C=y" >> prj.conf.tmp
+        echo "CONFIG_I2C=y" >> $PRJFILE
     fi
-    echo "export ZJS_I2C=y" >> zjs.conf.tmp
+    echo "export ZJS_I2C=y" >> $CONFFILE
     buffer=true;
 fi
 
@@ -276,67 +279,67 @@ if check_for_require grove_lcd || check_config_file ZJS_GROVE_LCD; then
     >&2 echo Using module: Grove LCD
     MODULES+=" -DBUILD_MODULE_GROVE_LCD"
     if [ $BOARD = "arduino_101" ]; then
-        echo "CONFIG_I2C=y" >> arc/prj.conf.tmp
-        echo "CONFIG_GROVE=y" >> arc/prj.conf.tmp
-        echo "CONFIG_GROVE_LCD_RGB=y" >> arc/prj.conf.tmp
-        echo "CONFIG_GROVE_LCD_RGB_INIT_PRIORITY=90" >> arc/prj.conf.tmp
+        echo "CONFIG_I2C=y" >> $ARCPRJFILE
+        echo "CONFIG_GROVE=y" >> $ARCPRJFILE
+        echo "CONFIG_GROVE_LCD_RGB=y" >> $ARCPRJFILE
+        echo "CONFIG_GROVE_LCD_RGB_INIT_PRIORITY=90" >> $ARCPRJFILE
     else
-        echo "CONFIG_I2C=y" >> prj.conf.tmp
-        echo "CONFIG_GROVE=y" >> prj.conf.tmp
-        echo "CONFIG_GROVE_LCD_RGB=y" >> prj.conf.tmp
-        echo "CONFIG_GROVE_LCD_RGB_INIT_PRIORITY=90" >> prj.conf.tmp
+        echo "CONFIG_I2C=y" >> $PRJFILE
+        echo "CONFIG_GROVE=y" >> $PRJFILE
+        echo "CONFIG_GROVE_LCD_RGB=y" >> $PRJFILE
+        echo "CONFIG_GROVE_LCD_RGB_INIT_PRIORITY=90" >> $PRJFILE
     fi
-    echo "export ZJS_GROVE_LCD=y" >> zjs.conf.tmp
+    echo "export ZJS_GROVE_LCD=y" >> $CONFFILE
 fi
 
 if check_for_require arduino101_pins || check_config_file ZJS_ARDUINO101_PINS; then
     >&2 echo Using module: A101 Pins
     MODULES+=" -DBUILD_MODULE_A101"
-    echo "export ZJS_ARDUINO101_PINS=y" >> zjs.conf.tmp
+    echo "export ZJS_ARDUINO101_PINS=y" >> $CONFFILE
 fi
 
 interval=$(grep "setInterval\|setTimeout\|setImmediate" $SCRIPT)
 if [ $? -eq 0 ] || check_config_file ZJS_TIMERS; then
     MODULES+=" -DBUILD_MODULE_TIMER"
-    echo "export ZJS_TIMERS=y" >> zjs.conf.tmp
+    echo "export ZJS_TIMERS=y" >> $CONFFILE
 fi
 
 if $buffer && [[ $MODULE != *"BUILD_MODULE_BUFFER"* ]]; then
     >&2 echo Using module: Buffer
     MODULES+=" -DBUILD_MODULE_BUFFER"
-    echo "export ZJS_BUFFER=y" >> zjs.conf.tmp
+    echo "export ZJS_BUFFER=y" >> $CONFFILE
 fi
 sensor=$(grep -E Accelerometer\|Gyroscope\|AmbientLightSensor $SCRIPT)
 if [ $? -eq 0 ] || check_config_file ZJS_SENSOR; then
     >&2 echo Using module: Sensor
     MODULES+=" -DBUILD_MODULE_SENSOR"
-    echo "export ZJS_SENSOR=y" >> zjs.conf.tmp
+    echo "export ZJS_SENSOR=y" >> $CONFFILE
     if [[ $BOARD = "arduino_101" ]] || [[ $DEV = "ashell" ]]; then
         bmi160=$(grep -E Accelerometer\|Gyroscope $SCRIPT)
         if [[ $? -eq 0 ]] || [[ $DEV = "ashell" ]]; then
-            echo "CONFIG_SENSOR=y" >> arc/prj.conf.tmp
-            echo "CONFIG_GPIO=y" >> arc/prj.conf.tmp
-            echo "CONFIG_SPI=y" >> arc/prj.conf.tmp
-            echo "CONFIG_SPI_0=n" >> arc/prj.conf.tmp
-            echo "CONFIG_SPI_1=n" >> arc/prj.conf.tmp
-            echo "CONFIG_SPI_SS_1_NAME=\"SPI_SS_1\"" >> arc/prj.conf.tmp
-            echo "CONFIG_BMI160=y" >> arc/prj.conf.tmp
-            echo "CONFIG_BMI160_NAME=\"bmi160\"" >> arc/prj.conf.tmp
-            echo "CONFIG_BMI160_SPI_PORT_NAME=\"SPI_SS_1\"" >> arc/prj.conf.tmp
-            echo "CONFIG_BMI160_SLAVE=1" >> arc/prj.conf.tmp
-            echo "CONFIG_BMI160_SPI_BUS_FREQ=88" >> arc/prj.conf.tmp
-            echo "CONFIG_BMI160_TRIGGER=y" >> arc/prj.conf.tmp
-            echo "CONFIG_BMI160_TRIGGER_OWN_THREAD=y" >> arc/prj.conf.tmp
+            echo "CONFIG_SENSOR=y" >> $ARCPRJFILE
+            echo "CONFIG_GPIO=y" >> $ARCPRJFILE
+            echo "CONFIG_SPI=y" >> $ARCPRJFILE
+            echo "CONFIG_SPI_0=n" >> $ARCPRJFILE
+            echo "CONFIG_SPI_1=n" >> $ARCPRJFILE
+            echo "CONFIG_SPI_SS_1_NAME=\"SPI_SS_1\"" >> $ARCPRJFILE
+            echo "CONFIG_BMI160=y" >> $ARCPRJFILE
+            echo "CONFIG_BMI160_NAME=\"bmi160\"" >> $ARCPRJFILE
+            echo "CONFIG_BMI160_SPI_PORT_NAME=\"SPI_SS_1\"" >> $ARCPRJFILE
+            echo "CONFIG_BMI160_SLAVE=1" >> $ARCPRJFILE
+            echo "CONFIG_BMI160_SPI_BUS_FREQ=88" >> $ARCPRJFILE
+            echo "CONFIG_BMI160_TRIGGER=y" >> $ARCPRJFILE
+            echo "CONFIG_BMI160_TRIGGER_OWN_THREAD=y" >> $ARCPRJFILE
         fi
         grovelight=$(grep -E AmbientLightSensor $SCRIPT)
         if [[ $? -eq 0 ]] || [[ $DEV = "ashell" ]]; then
             MODULES+=" -DBUILD_MODULE_SENSOR_LIGHT"
-            echo "CONFIG_ADC=y" >> arc/prj.conf.tmp
+            echo "CONFIG_ADC=y" >> $ARCPRJFILE
             # Workaround for the Zephyr issue ZEP-1882: ADC doesn't work with
             # SDK 0.9 due to some compiler optimization, so enable debug mode.
             if [ $SDK_VERSION = "0.9" ]; then
                 >&2 echo Warning: Zephyr SDK 0.9 detected, compiling in debug mode!
-                echo "CONFIG_DEBUG=y" >> arc/prj.conf.tmp
+                echo "CONFIG_DEBUG=y" >> $ARCPRJFILE
             fi
         fi
     fi
@@ -346,8 +349,8 @@ console=$(grep console $SCRIPT)
 if [ $? -eq 0 ] || check_config_file ZJS_CONSOLE && [[ $MODULE != *"BUILD_MODULE_CONSOLE"* ]]; then
     >&2 echo Using module: Console
     MODULES+=" -DBUILD_MODULE_CONSOLE"
-    echo "export ZJS_CONSOLE=y" >> zjs.conf.tmp
+    echo "export ZJS_CONSOLE=y" >> $CONFFILE
 fi
 
-echo "# ---------------------------" >> zjs.conf.tmp
+echo "# ---------------------------" >> $CONFFILE
 echo $MODULES
