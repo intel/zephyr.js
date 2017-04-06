2bab6e1dbe817ae1257534faff7785118badabb3
James Prestwood <james.prestwood@intel.com>
2017-04-06 09:11:02 -0700

    [dgram] Added ip route info to IPv6 sample (#973)
    Signed-off-by: James Prestwood <james.prestwood@intel.com>

--- samples/UDPEchoServ6.js
+++ samples/UDPEchoServ6.js
@@ -2,9 +2,18 @@
 
 // Sample for "dgram" module implementing an IPv6 UDP echo server
 // and showing use of offset and length parameters to send() call,
-// and error callback. To run it on QEMU, you'll need additional
+// and error callback.
+
+// To run it on a BLE/6lowpan device (A101/nRF52) you will need
+// to first connect to the device using BLE, then add a new IP
+// route to the bt0 interface:
+
+// ip -6 route add 2001:db8::/64 dev bt0
+
+// To run it on QEMU, you'll need additional
 // setup for networking to work, follow
 // https://www.zephyrproject.org/doc/subsystems/networking/qemu_setup.html
+
 // From the host, you can generate test UDP traffic using "netcat"
 // tool:
 //
