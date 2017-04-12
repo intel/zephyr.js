e453684851dc808ae2dae71ecfe69a5576cf94e8
Geoff Gustafson <geoff@linux.intel.com>
2017-04-11 17:30:25 -0700

    [buffer] Add magic number to Buffer to verify we've been given one
    Also, make a private structure for the buffer handle to hold values
    only the buffer implementation needs to know about.
    
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- src/zjs_buffer.h
+++ src/zjs_buffer.h
@@ -13,10 +13,8 @@ void zjs_buffer_cleanup();
 
 // FIXME: We should make this private and have accessor methods
 typedef struct zjs_buffer {
-    jerry_value_t obj;  // weak reference
     uint8_t *buffer;
     uint32_t bufsize;
-    struct zjs_buffer *next;
 } zjs_buffer_t;
 
 zjs_buffer_t *zjs_buffer_find(const jerry_value_t obj);
