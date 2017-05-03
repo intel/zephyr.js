3b9d126a766162eee74b84a9684e97b8b1305876
James Prestwood <james.prestwood@intel.com>
2017-05-03 12:23:19 -0700

    [samples] OCF sample using multiple resources
    Signed-off-by: James Prestwood <james.prestwood@intel.com>

---
+++ samples/OcfMultiSensor.js
@@ -0,0 +1,60 @@
+// Copyright (c) 2017, Intel Corporation.
+
+var ocf = require('ocf');
+var server = ocf.server;
+
+var aio = require('aio');
+var pins = require("arduino101_pins");
+
+var light = aio.open({ device: 0, pin: pins.A0 });
+var temp = aio.open({ device: 0, pin: pins.A1 });
+
+console.log("Started OCF server");
+
+var lightResource = {
+    light: null
+};
+
+var tempResource = {
+    temp: null
+};
+
+var lightInit = {
+    resourcePath: "/a/light",
+    resourceTypes: ["core.light"],
+    interfaces: ["/oic/if/rw"],
+    discoverable: true,
+    observable: true,
+    properties: lightResource
+};
+
+var tempInit = {
+    resourcePath: "/a/temp",
+    resourceTypes: ["core.temp"],
+    interfaces: ["/oic/if/rw"],
+    discoverable: true,
+    observable: true,
+    properties: tempResource
+};
+
+server.register(lightInit).then(function(resource) {
+    console.log("Light resource registered");
+});
+
+server.register(tempInit).then(function(resource) {
+    console.log("Temperature resource registered");
+});
+
+server.on('retrieve', function(request, observe) {
+    if (request.target.resourcePath == "/a/light") {
+        lightResource.light = light.read();
+        request.respond(lightResource);
+    } else if (request.target.resourcePath == "/a/temp") {
+        tempResource.temp = temp.read();
+        request.respond(tempResource);
+    } else {
+        console.log("Resource requested does not exist");
+    }
+});
+
+ocf.start();
