2b3c080f5bdaf2ed818f3a6315139cd5f4323626
Cui Yan <yanx.cui@intel.com>
2017-05-10 03:11:56 +0800

    [ble] Add 6LoWPAN test on ocf server (#1118)
    Signed-off-by: cuiyanx <yanx.cui@intel.com>

--- tests/test-ocfserver-bleaddr-manual.js
+++ tests/test-ocfserver-bleaddr-manual.js
@@ -1,6 +1,13 @@
 // Copyright (c) 2017, Intel Corporation.
 
-console.log("Test setBleAddress()");
+// Build this test case and check ble address as "F1:E2:D3:C4:B5:A6",
+//   and check default device name as "ZJS Device".
+// Build this test case with BLE_ADDR flag again:
+//     make JS=tests/test-ocfserver-bleaddr-manual.js BLE_ADDR="F2:E3:D4:C5:B6:A7"
+// Build this test case with DEVICE_NAME flag again:
+//     make JS=tests/test-ocfserver-bleaddr-manual.js DEVICE_NAME="MyServer"
+
+console.log("Test BLE with ocf server");
 
 var ocf = require("ocf");
 
@@ -8,8 +15,17 @@ var server = ocf.server;
 
 var BleAddress = "F1:E2:D3:C4:B5:A6";
 ocf.setBleAddress(BleAddress);
-console.log("Please connect server with Bluetooth");
-console.log("OCF BLE address: expected result " + BleAddress);
+
+console.log("Please connect server with Bluetooth....\n");
+console.log("If build with 'BLE_ADDR' flag:");
+console.log("    OCF BLE address: expected result 'F2:E3:D4:C5:B6:A7'");
+console.log("If not:");
+console.log("    OCF BLE address: expected result '" + BleAddress + "'\n");
+
+console.log("If build with 'DEVICE_NAME' flag:");
+console.log("    OCF BLE address: expected result 'MyServer'");
+console.log("If not:");
+console.log("    OCF BLE address: expected result 'ZJS Device'");
 
 var TestProperties = {
     state: true
@@ -34,3 +50,5 @@ server.register(resourceInit).then(function(resource) {
 }).catch(function(error) {
     console.log("OCFServer error: " + error.name);
 });
+
+ocf.start();
