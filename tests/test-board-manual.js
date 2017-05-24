91387cf3f371bc23a948f05baa3292c0f144ef85
Cui Yan <yanx.cui@intel.com>
2017-05-24 16:23:18 +0800

    [tests] Add test case for board APIs
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

---
+++ tests/test-board-manual.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2017, Intel Corporation.
+
+console.log("Test board API");
+var board = require("board");
+
+var expectedName = [
+    ["   Arduino 101", "arduino_101"],
+    ["     FRDM-K64F", "frdm_k64f"],
+    ["nRF52-PCA10040", "nrf52_pca10040"],
+    ["   Arduino Due", "arduino_due"],
+    [" Nucleo F411RE", "nucleo_f411re"],
+    ["         Linux", "linux (partially simulating arduino_101)"],
+    ["        Others", "unknown"]
+]
+
+console.log("\n\rBoard name: " + board.name);
+console.log("expected result:");
+for (var i = 0; i < expectedName.length; i++) {
+    console.log("    " + expectedName[i][0] + " : " + expectedName[i][1]);
+}
+
+console.log("\n\rBoard version: " + board.version);
+console.log("expected result '0.1'");
