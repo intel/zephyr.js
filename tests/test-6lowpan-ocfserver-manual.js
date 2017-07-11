0b8c6c0207549eddc9099cf5c92215e1c83f1c6d
Cui Yan <yanx.cui@intel.com>
2017-07-11 10:15:31 +0800

    [test] Remove OCFObject.setBleAddress test point
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

--- tests/test-6lowpan-ocfserver-manual.js
+++ tests/test-6lowpan-ocfserver-manual.js
@@ -1,26 +1,23 @@
 // Copyright (c) 2017, Intel Corporation.
 
-// Build this test case and check ble address as "F1:E2:D3:C4:B5:A6",
-//   and check default device name as "ZJS Device".
+// Build this test case and check random ble address,
+//   and check default device name as "ZJS Device":
+//     make JS=tests/test-6lowpan-ocfserver-manual.js ROM=256
 // Build this test case with BLE_ADDR flag again:
-//     make JS=tests/test-ocfserver-bleaddr-manual.js BLE_ADDR="F2:E3:D4:C5:B6:A7"
+//     make JS=tests/test-6lowpan-ocfserver-manual.js ROM=256 BLE_ADDR="F2:E3:D4:C5:B6:A7"
 // Build this test case with DEVICE_NAME flag again:
-//     make JS=tests/test-ocfserver-bleaddr-manual.js DEVICE_NAME="MyServer"
+//     make JS=tests/test-6lowpan-ocfserver-manual.js ROM=256 DEVICE_NAME="MyServer"
 
-console.log("Test BLE with ocf server");
+console.log("Test 6LoWPAN APIs with ocf server");
 
 var ocf = require("ocf");
-
 var server = ocf.server;
-
-var BleAddress = "F1:E2:D3:C4:B5:A6";
-ocf.setBleAddress(BleAddress);
 
 console.log("Please connect server with Bluetooth....\n");
 console.log("If build with 'BLE_ADDR' flag:");
 console.log("    OCF BLE address: expected result 'F2:E3:D4:C5:B6:A7'");
 console.log("If not:");
-console.log("    OCF BLE address: expected result '" + BleAddress + "'\n");
+console.log("    OCF BLE address: expected result 'random'\n");
 
 console.log("If build with 'DEVICE_NAME' flag:");
 console.log("    OCF BLE address: expected result 'MyServer'");
@@ -37,8 +34,6 @@ var resourceInit = {
     interfaces: ["/oic/if/rw"],
     discoverable: true,
     observable: true,
-    secure: true,
-    slow: false,
     properties: TestProperties
 }
 
@@ -46,9 +41,9 @@ server.on("retrieve", function onRetrieve(request, observe) {});
 server.on("update", function onUpdate(request) {});
 
 server.register(resourceInit).then(function(resource) {
-    server.notify(resource);
+    console.log("Registered OCF server");
 }).catch(function(error) {
-    console.log("OCFServer error: " + error.name);
+    console.log("OCFServer error: " + error.name + ":" + error.message);
 });
 
 ocf.start();
