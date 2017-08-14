24395258ea9cbbbb1a1a5f4253a4ce5e1d7a187a
Geoff Gustafson <geoff@linux.intel.com>
2017-08-14 15:05:35 -0700

    [websockets] Increase net buf data size to work around issues (#1460)
    There appears to be a bug in Zephyr's net buf fragmentation code,
    or else this shouldn't make a difference. Need to look into that
    further.
    
    Also, clean up some code and output in the node sample code.
    
    Fixes an unreported issue where the websocket sample doesn't work at
    all on A101 w/ Ethernet currently.
    
    Fixes #1385
    
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- samples/websockets/NodeWebSocketClient.js
+++ samples/websockets/NodeWebSocketClient.js
@@ -2,17 +2,19 @@
 
 var WebSocket = require('ws');
 
+console.log("Starting WebSocket Client...");
+
 var ws = new WebSocket('ws://[2001:db8::1]:8080', ["first", "my_ws_protocol", "last"] );
 ws.on('connection', function(s) {
     console.log("CONNECTION");
 });
 ws.on('open', function open() {
-  ws.send('first packet');
+    ws.send('first packet');
 });
 
 ws.on('message', function(data, flags) {
-  console.log(data);
-  ws.send(data);
+    console.log(data);
+    ws.send(data);
 });
 
 ws.on('ping', function(data, flags) {
