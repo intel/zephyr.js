fd7818683624e8b7c9af697ed8fd7de5935adf68
Brian J Jones <brian.j.jones@intel.com>
2017-10-09 11:39:37 -0700

    Adding zjs_event_create_prototype to zjs_event.h
    Signed-off-by: Brian J Jones <brian.j.jones@intel.com>

--- src/zjs_event.h
+++ src/zjs_event.h
@@ -192,7 +192,7 @@ void zjs_release_args(void *unused, jerry_value_t argv[], u32_t argc);
  * @return              Event constructor
  */
 jerry_value_t zjs_event_init();
-
+void zjs_event_create_prototype();
 /** Release resources held by the event module */
 void zjs_event_cleanup();
 
