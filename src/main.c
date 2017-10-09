be3ebf7c43843a99eb7229d7c3700628de666291
Jimmy Huang <jimmy.huang@intel.com>
2017-10-09 12:05:33 -0700

    [ble] Fixed ble in Ashell
    This fixes the issue where bluetooth doesn't work in Ashell,
    since when running in Ashell, bt_enable() cannot be called until
    the Bluetooth module is initialized.  This also fixes the error message
    showing up when IDE is launched.
    
    Fixes #1502
    
    Signed-off-by: Jimmy Huang <jimmy.huang@intel.com>

--- src/main.c
+++ src/main.c
@@ -108,14 +108,14 @@ u8_t process_cmd_line(int argc, char *argv[])
 }
 #else
 #ifdef BUILD_MODULE_BLE
+#ifndef ZJS_ASHELL
 // INTERRUPT SAFE FUNCTION: No JerryScript VM, allocs, or release prints!
 static void ble_bt_ready(int err)
 {
     DBG_PRINT("bt_ready() is called [err %d]\n", err);
-#ifdef BUILD_MODULE_BLE
     zjs_ble_emit_powered_event();
-#endif
 }
+#endif
 #endif
 #endif
 
@@ -233,6 +233,7 @@ if (start_debug_server) {
     }
 
 #ifndef ZJS_LINUX_BUILD
+#ifndef ZJS_ASHELL  // Ashell will call bt_enable when module is loaded
 #ifndef BUILD_MODULE_OCF  // OCF will call bt_enable() itself
 #ifdef BUILD_MODULE_BLE
     if (bt_enable(ble_bt_ready)) {
@@ -248,6 +249,7 @@ if (start_debug_server) {
     ipss_init();
     ipss_advertise();
     net_ble_enabled = 1;
+#endif
 #endif
 #endif
 #endif
