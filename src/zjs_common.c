b16c1012c7c871a0f32e22e99c3c9fd8f1f1413f
Jimmy Huang <jimmy.huang@intel.com>
2017-10-31 16:34:55 -0700

    use zjs_port_timer_get_uptime instead
    Signed-off-by: Jimmy Huang <jimmy.huang@intel.com>

--- src/zjs_common.c
+++ src/zjs_common.c
@@ -27,49 +27,22 @@ char *zjs_shorten_filepath(char *filepath)
 }
 
 #ifdef DEBUG_BUILD
-#ifdef ZJS_LINUX_BUILD
-#include <time.h>
-
-static u8_t init = 0;
-static int seconds = 0;
-#else
 static int start = 0;
-#endif
 
 int zjs_get_sec(void)
 {
-#ifdef ZJS_LINUX_BUILD
-    struct timespec now;
-    clock_gettime(CLOCK_MONOTONIC, &now);
-    if (!init) {
-        seconds = now.tv_sec;
-        init = 1;
-    }
-    return now.tv_sec - seconds;
-#else
     if (!start) {
         start = zjs_port_timer_get_uptime() / 1000;
     }
     return (zjs_port_timer_get_uptime() / 1000) - start;
-#endif
 }
 
 int zjs_get_ms(void)
 {
-#ifdef ZJS_LINUX_BUILD
-    struct timespec now;
-    clock_gettime(CLOCK_MONOTONIC, &now);
-    if (!init) {
-        seconds = now.tv_sec;
-        init = 1;
-    }
-    return (now.tv_nsec / 1000000);
-#else
     if (!start) {
         start = zjs_port_timer_get_uptime() / 1000;
     }
     return zjs_port_timer_get_uptime() - (zjs_get_sec() * 1000);
-#endif
 }
 
 #endif  // DEBUG_BUILD
