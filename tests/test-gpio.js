bc47cc2e032f7f3990c53051cbd5723fbf2d209a
Cui Yan <yanx.cui@intel.com>
2017-11-20 17:41:58 +0800

    [tests] Add test points for gpio APIs
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

--- tests/test-gpio.js
+++ tests/test-gpio.js
@@ -4,54 +4,107 @@
 
 var gpio = require("gpio");
 var assert = require("Assert.js");
-
-if (gpio.mock) {
-    gpio.wire(gpio.open(7), gpio.open(10));
-}
-else {
-    // NOTE: changed from pin 8 to 10 so this test can work on k64f too
-    console.log("Hardware setup: Wire pin 7 to pin 10");
-}
-
-var pinA, pinB, aValue, bValue;
+var board = require("board");
 
 // test GPIO open
+if (gpio.mock) {
+    // support Linux
+    gpio.wire(gpio.open(7), gpio.open(10));
+} else {
+    console.log("Wire pin 7 to pin 10");
+}
+
+if (board.name === "arduino_101" ||
+    board.name === "linux (partially simulating arduino_101)") {
+    var pinNames = [
+        [4, "number"],
+        ["4", "string"],
+        ["GPIO_0.4", "string"],
+        ["IO4", "string"],
+        ["LED0", "string"]
+    ];
+    for (var i = 0; i < pinNames.length; i++) {
+        var pinOpen = gpio.open(pinNames[i][0]);
+
+        assert(pinOpen !== null && typeof pinOpen === "object",
+              "open: defined pin with " + pinNames[i][1] +
+               " '" + pinNames[i][0] + "'");
+    }
+} else {
+    var pinNames = [
+        [4, "number"],
+        ["4", "string"],
+        ["GPIO_0.4", "string"],
+        ["D4", "string"],
+        ["LEDR", "string"],
+        ["LEDG", "string"],
+        ["LEDB", "string"],
+        ["SW2", "string"],
+    ];
+    for (var i = 0; i < pinNames.length; i++) {
+        var pinOpen = gpio.open(pinNames[i][0]);
+
+        assert(pinOpen !== null && typeof pinOpen === "object",
+              "open: defined pin with " + pinNames[i][1] +
+               " '" + pinNames[i][0] + "'");
+    }
+}
+
+var pinA, pinB, writeValue, aValue, bValue, WRFlag;
+
 pinA = gpio.open(7);
-pinB = gpio.open({pin: 10, mode: "in", edge: "none"});
-
 assert(pinA != null && typeof pinA == "object",
-      "open: defined pin and default as 'out' direction");
+      "open: default value - mode: 'out' activeLow: 'true'");
 
+pinB = gpio.open({pin: 10, mode: "in", edge: "none", activeLow: false});
 assert(pinB != null && typeof pinB == "object",
-      "open: defined pin with direction 'in'");
+      "open: set value - mode: 'in' edge: 'none' activeLow: 'false'");
 
 assert.throws(function () {
     gpio.open(1024);
 }, "open: invalid pin");
 
-// test GPIOPin read and write
+// test activeLow
 pinA.write(1);
 bValue = pinB.read();
-assert(bValue, "gpiopin: write and read");
+assert(bValue === 1, "activeLow: false");
 
-// reading an output pin may not be valid; this is probably a bad test
-aValue = pinA.read();
-assert(aValue, "gpiopin: read output pin");
+// test GPIOPin read and write
+writeValue = 1;
+WRFlag = true;
 
+for (var i = 0; i < 10; i++) {
+    writeValue = 1 - writeValue;
+
+    pinA.write(writeValue);
+    bValue = pinB.read();
+
+    if (writeValue !== bValue) {
+        WRFlag = false;
+    }
+}
+assert(WRFlag, "gpiopin: write and read 10 times");
+
+pinA.write(1);
 pinB.write(0);
 bValue = pinB.read();
 assert(bValue, "gpiopin: write input pin");
 
 assert.throws(function () {
-    // booleans allowed temporarily as deprecation path, so try another value
-    pinA.write("1");
+    pinA.write(pinA);
 }, "gpiopin: write invalid argument");
 
-// test activeLow
-pinB = gpio.open({pin: 10, mode: "in", activeLow: true});
-pinA.write(0);
-bValue = pinB.read();
-assert(bValue, "activeLow: true");
+// test GPIOPin close
+pinA.write(1);
+pinB.close();
+assert.throws(function () {
+    bValue = pinB.read();
+}, "close: not be used for reading");
+
+pinA.close();
+assert.throws(function () {
+    pinA.write(1);
+}, "close: not be used for writing");
 
 // test GPIOPin onchange
 var changes = [
